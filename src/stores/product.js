e1667562dbc5b1eae93f501b1820537abcc0ddb6
motya <motya228228@mail.ru>
2024-11-28 09:13:59 +0500

    add details page

--- src/stores/product.js
+++ src/stores/product.js
@@ -9,7 +9,7 @@ export const useProductStore = defineStore('product', () =>
         img: 'https://static.shop.kz/upload/resize_cache/iblock/065/gcgsznwq4suzskq5b9ri2x4mdaggx6sq/450_450_1/176279x3.webp',
         price: 63,
         category: 'Motherboard',
-        description: 'AAaaaaa',
+        description: 'Материнская плата GIGABYTE H510M K V2 совместима с процессорами Intel Core в исполнении LGA 1200 10 и 11 поколения. Она выполнена в типоразмере Micro-ATX, поэтому подходит для сборки аппаратной платформы в условиях ограниченного пространства. Для размещения оперативной памяти есть 2 слота DIMM стандарта DDR4. Задействовать видеокарту позволяют слоты расширения PCI-E x16 и PCI-E x1.',
         publishAt: '27.11.2024'
     },
     {
@@ -18,7 +18,7 @@ export const useProductStore = defineStore('product', () =>
         img: 'https://static.shop.kz/upload/resize_cache/iblock/ddb/f5lg2lvl0ld7xkdr9ict2rqh43iy7e48/450_450_1/183165_02_2.webp',
         price: 740,
         category: 'CPU',
-        description: 'AAaaaaa',
+        description: 'AMD Ryzen 9 9950X — это 16-ядерный процессор для ПК, который входит в линейку Ryzen 9 и использует архитектуру Zen 5 (Granite Ridge) с разъемом AM5.',
         publishAt: '27.11.2024'
         
     },
@@ -28,7 +28,7 @@ export const useProductStore = defineStore('product', () =>
         img: 'https://c.dns-shop.kz/thumb/st1/fit/500/500/b6f56e24852bb3b1ffc4fe9ce4da69a3/291e3d866c55a4a55843d59380a9a2fe063f05290e8df4b95efb35ddf6b12e0b.jpg.webp',
         price: 440,
         category: 'GPU',
-        description: 'AAaaaaa',
+        description: 'Видеокарта MSI GeForce GTX 1650 VENTUS XS OC [GTX 1650 VENTUS XS 4G OC] – популярная модель от всемирно известного производителя высокотехнологичных компьютерных комплектующих. Видеокарта предназначена для монтажа в системные блоки, преимущественно используемые для игр. Несмотря на высокий уровень производительности, устройство компактно: длина видеоадаптера равна лишь 177 мм. Величина этого параметра свидетельствует об увеличении свободы выбора корпуса для сборки системы. ',
         publishAt: '27.11.2024'
         
     },
@@ -38,7 +38,7 @@ export const useProductStore = defineStore('product', () =>
         img: 'https://c.dns-shop.kz/thumb/st4/fit/500/500/7552b6341dea70a9dea9102b6c2609e8/93b717156b88cba868730da8338b1b4dcca873ec855fe4a603a5cf92e7489a5a.jpg.webp',
         price: 100,
         category: 'RAM',
-        description: 'AAaaaaa',
+        description: 'Оперативная память Kingston FURY Beast Black способна обеспечить высокие показатели производительности и скорости в составе игрового компьютера. Два модуля DDR4 объемом по 8 ГБ и частотой в пределах 3200 МГц помогают ускорить запуск компьютерных игр, повысить отзывчивость системы при редактировании видео и визуализации. Kingston FURY Beast выделяется стилизованным алюминиевым радиатором черного цвета.',
         publishAt: '27.11.2024'
         
     }
@@ -61,7 +61,13 @@ export const useProductStore = defineStore('product', () =>
         }
     })
 
+    const findProductbyID = computed(() => {
+        return (id) => {
+            return products.value.find((product) => product.id == id)
+        }
+    })
+
     return {
-        products, productsFilteredbyCategoryNameList
+        products, productsFilteredbyCategoryNameList, findProductbyID
     }
 })
